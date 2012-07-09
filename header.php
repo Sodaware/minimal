555ad3901b7c472b30055925c010c84ba022dd1d
Phil Newton <phil@sodaware.net>
2012-07-09 10:12:06 -0400

    Changed title seperator to pipe.

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 <head>
 	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 
-	<title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
+	<title><?php wp_title('|', true, 'right'); ?> <?php bloginfo('name'); ?></title>
 	
 	<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
 	<link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
