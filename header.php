13a6655dbd740a25391e4e1167198a21c6e4bc94
Phil Newton <phil@sodaware.net>
2016-09-14 20:01:57 -0400

    Cleanup `header.php` indentation

--- header.php
+++ header.php
@@ -2,25 +2,25 @@
 <html>
 
 <head>
-	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+	<meta http-equiv="Content-Type" content="<?php bloginfo( 'html_type' ) ?>; charset=<?php bloginfo( 'charset' ) ?>" />
 
-	<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
-	<link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
-	<link rel="alternate" type="application/atom+xml" title="Atom 1.0" href="<?php bloginfo('atom_url'); ?>" />
+	<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo( 'rss2_url' ) ?>" />
+	<link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo( 'rss_url' ) ?>" />
+	<link rel="alternate" type="application/atom+xml" title="Atom 1.0" href="<?php bloginfo( 'atom_url' ) ?>" />
 
 	<!--[if IE]>
-		<script src="<?php bloginfo('stylesheet_directory'); ?>/js/modernizr.js"></script>
+		<script src="<?php bloginfo( 'stylesheet_directory' ) ?>/js/modernizr.js"></script>
 	<![endif]-->
 
-	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-	
-	<?php wp_get_archives('type=monthly&format=link'); ?>
-	
-	<?php wp_head(); ?>
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ) ?>" />
+
+	<?php wp_get_archives( 'type=monthly&format=link' ) ?>
+
+	<?php wp_head() ?>
 </head>
 
 <body>
 
 <p class="to_content"><a href="#content-container">&darr; Skip to Content</a></p>
-	
-<?php get_sidebar(); ?>
+
+<?php get_sidebar() ?>
